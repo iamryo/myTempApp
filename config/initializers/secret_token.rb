f6faa5fc6b3634fae2cd235ff442f68483de106b
railsException@yahoo.com <cts_janardan@yaho.com>
2014-01-23 23:44:42 -0600

    second commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyTempApp::Application.config.secret_token = '0246df9e661fd1393f9c3ddc0a781fa6e58d420d1a4f402ed40fa2f27497acae3e39e1cc05d8e7682a38611975139f68df162bb6afd42e5073d5a4ce4516375c'
